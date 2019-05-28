da71ead8e7522e3ca5a02937657d33ea4ac9c89c
Navid Mashinchi <nmashinchi@gmail.com>
2019-05-27 17:22:09 -0700

    Adding Head Shot Not Working

---
+++ spec/integration/about_spec.js
@@ -0,0 +1,17 @@
+const request = require ("request");
+const server = require ("../../src/server");
+const base = "http://localhost:3000/about";
+
+describe("routes : about", () => {
+
+    describe("GET /about", () => {
+
+        it("should return status code 200", (done) => {
+
+            request.get(base, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                done();
+            });
+        });
+    });
+});
--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,8 @@
 module.exports = {
     init(app){
       const landingRoutes = require("../routes/landing");
+      const aboutRoutes = require("../routes/about");
       app.use(landingRoutes);
+      app.use(aboutRoutes);
     }
   }

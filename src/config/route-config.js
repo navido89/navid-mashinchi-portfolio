355aea213c86c5f873bc3379fb84bb98ba947273
Navid Mashinchi <nmashinchi@gmail.com>
2019-06-04 19:16:49 -0700

    worked on skills page

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,7 +2,9 @@ module.exports = {
     init(app){
       const landingRoutes = require("../routes/landing");
       const aboutRoutes = require("../routes/about");
+      const skillsRoutes = require("../routes/skills");
       app.use(landingRoutes);
       app.use(aboutRoutes);
+      app.use(skillsRoutes);
     }
   }

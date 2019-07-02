45413db9fd690bc68d93bf53fd0ca7c2c90d5f2a
Navid Mashinchi <nmashinchi@gmail.com>
2019-07-02 14:13:01 -0700

    Corrected Mentor Suggestion

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,7 +1,6 @@
 module.exports = {
     init(app){
       const landingRoutes = require("../routes/landing");
-      const aboutRoutes = require("../routes/about");
       const skillsRoutes = require("../routes/skills");
       const workRoutes = require("../routes/work");
       const contactRoutes = require("../routes/contact");
@@ -11,7 +10,6 @@ module.exports = {
       app.use(contactRoutes);
       app.use(workRoutes);
       app.use(landingRoutes);
-      app.use(aboutRoutes);
       app.use(skillsRoutes);
     }
   }
--- src/routes/about.js
+++
@@ -1,7 +0,0 @@
-const express = require ("express");
-const router = express.Router();
-const aboutController = require("../controllers/aboutController");
-
-router.get("/about", aboutController.index);
-
-module.exports = router;
